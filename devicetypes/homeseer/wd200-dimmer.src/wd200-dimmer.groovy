191a9af122d7f2531ddf33a88405e74a961ba574
samuelkadolph <samuel@kadolph.com>
2018-03-24 16:42:28 -0400

    WIP

--- devicetypes/homeseer/wd200-dimmer.src/wd200-dimmer.groovy
+++ devicetypes/homeseer/wd200-dimmer.src/wd200-dimmer.groovy
@@ -6,6 +6,24 @@
  *
  *  Buttons
  *
+ *
+ *  Parameters
+ *    Number  Description
+ *    3       Bottom LED operation
+ *    4       Paddle load orientation
+ *    11      Ramp rate for remote control
+ *    12      Ramp rate for local control
+ *    13      Mode of operation
+ *    14      Normal mode LED color
+ *    21
+ *    22
+ *    23
+ *    24
+ *    25
+ *    26
+ *    27
+ *    30
+ *    31
  */
 
 metadata {
@@ -34,12 +52,43 @@ metadata {
         }
       }
 
+      standardTile("tapUp2", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▲▲", action: "tapUp2", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("tapDown2", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▼▼", action: "tapDown2", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("tapUp3", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▲▲▲", action: "tapUp3", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("tapDown3", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▼▼▼", action: "tapDown3", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("tapUp4", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▲▲▲▲", action: "tapUp4", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("tapDown4", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▼▼▼▼", action: "tapDown4", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("tapUp5", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▲▲▲▲▲", action: "tapUp5", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("tapDown5", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Tap ▼▼▼▼▼", action: "tapDown5", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("holdUp", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Hold ▲", action: "holdUp", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+      standardTile("holdDown", "device.button", width: 2, height: 2, decoration: "flat") {
+        state "default", label: "Hold ▼", action: "holdDown", icon: "st.Home.home30", backgroundColor: "#FFFFFF"
+      }
+
       standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
         state "default", label:'', action:"refresh.refresh", icon:"st.secondary.configure"
       }
 
       main "switch"
-      details "switch", "refresh"
+      details "switch", "tapUp2","tapUp3","tapUp4","tapUp5","holdUp","tapDown2","tapDown3","tapDown4","tapDown5","holdDown", "refresh"
     }
   }
 }
