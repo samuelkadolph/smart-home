920d58163fc9154ba9ab7ee0e4cc129462c66f4d
samuelkadolph <samuel@kadolph.com>
2018-04-14 10:39:49 -0400

    Add description and copyright

--- devicetypes/fortrezz/mimolite-fireplace/mimolite-fireplace.groovy
+++ devicetypes/fortrezz/mimolite-fireplace/mimolite-fireplace.groovy
@@ -1,7 +1,29 @@
 /*
- *  FortrezZ MIMOLite
+ * MIMOLite Fireplace
  *
- *  https://www.fortrezz.com/io-modules
+ * A SmartThings Device Handler that uses a FortrezZ MIMOLite (see https://www.fortrezz.com/io-modules) to remotely
+ * control a gas fireplace while preserving the functionality of the low voltage switch commonly installed.
+ *
+ * Copyright (c) 2018 Samuel Kadolph
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 metadata {
@@ -75,7 +97,6 @@ def parse(String description) {
     return null
   }
 }
-
 
 def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
   log.debug cmd
