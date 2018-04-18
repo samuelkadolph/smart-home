fac0bdaadd5aecb73e35534382ca875021cb6c30
samuelkadolph <samuel@kadolph.com>
2018-04-18 00:47:14 -0400

    Typo

--- devicetypes/generic/zwave-repeater.src/zwave-repeater.groovy
+++ devicetypes/generic/zwave-repeater.src/zwave-repeater.groovy
@@ -33,7 +33,7 @@ metadata {
     fingerprint mfr: "0086", prod: "0104", model: "0075", deviceJoinName: "Z-Wave Repeater" // Aeotec Range Extender 6
 
     tiles(scale: 2) {
-      multiAttributeTile(name: "status", type: "generic", width: 6, height: 4 icon: "https://cdn.rawgit.com/samuelkadolph/smart-home/39a2cd66/icons/zwave-repeater.png") {
+      multiAttributeTile(name: "status", type: "generic", width: 6, height: 4, icon: "https://cdn.rawgit.com/samuelkadolph/smart-home/39a2cd66/icons/zwave-repeater.png") {
         tileAttribute("device.status", key: "PRIMARY_CONTROL") {
           attributeState "unknown", label: "UNKNOWN", backgroundColor:"#FFFFFF"
           attributeState "online", label: "ONLINE", backgroundColor: "#00A0DC"
