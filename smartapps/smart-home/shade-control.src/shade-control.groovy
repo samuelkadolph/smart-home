1823a71b49d87ebd22cb6cefeb9c18b751bd3af3
Samuel Kadolph <samuel@kadolph.com>
2019-01-19 21:25:29 -0500

    WIP

--- smartapps/smart-home/shade-control.src/shade-control.groovy
+++ smartapps/smart-home/shade-control.src/shade-control.groovy
@@ -1,41 +1,80 @@
 /**
  *  Shade Control
+ *    SmartThings SmartApp that controls window shades with the buttons on a switch.
  *
- *  Copyright 2019 Samuel Kadolph
+ *  Copyright (c) 2019 Samuel Kadolph
  *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 definition(
-    name: "Shade Control",
-    namespace: "smart-home",
-    author: "Samuel Kadolph",
-    description: "a",
-    category: "",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+  name: "Shade Control",
+  namespace: "smart-home",
+  author: "Samuel Kadolph",
+  description: "Control your shades with a switch",
+  category: "",
+  iconUrl: "http://cdn.device-icons.smartthings.com/Home/home9-icn.png",
+  iconX2Url: "http://cdn.device-icons.smartthings.com/Home/home9-icn@2x.png",
+  iconX3Url: "http://cdn.device-icons.smartthings.com/Home/home9-icn@3x.png"
+)
 
 
 preferences {
-	section("Title") {
-		// TODO: put inputs here
-	}
+  section("Shades to control") {
+    input "shades", "capability.windowShade", multiple: true
+  }
+
+  section("Controlled by these switches") {
+    input "switches", "capability.switch", multiple: true
+  }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
+  log.debug("installed() settings: ${settings}")
 
-	initialize()
+  initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
+  log.debug("updated() settings: ${settings}")
 
-	unsubscribe()
-	initialize()
+  unsubscribe()
+  initialize()
 }
 
-def initialize() {
-	// TODO: subscribe to attributes, devices, locations, etc.
+def handleButtonEvent(event) {
+  def data = new groovy.json.JsonSlurper().parseText(event.data)
+
+  log.debug("handleButtonEvent() event: ${event.value} ${data.buttonNumber}")
+
+  if (event.value == "pushed") {
+    if (data.buttonNumber == 5) {
+      log.debug("Opening shades ${shades}")
+      shades.on()
+    } else if (data.buttonNumber == 6) {
+      log.debug("Closing shades ${shades}")
+      shades.off()
+    }
+  }
 }
 
-// TODO: implement event handlers
+private def initialize() {
+  subscribe(switches, "button", handleButtonEvent)
+}
